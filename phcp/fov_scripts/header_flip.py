6f03e9a5a39c6f24794ca1073453752996ddd619
Yann Leprince <yann.leprince@cea.fr>
2025-03-03 11:26:01 +0100

    Clarify path handling to fix phcp-header-flip

--- phcp/fov_scripts/header_flip.py
+++ phcp/fov_scripts/header_flip.py
@@ -1,6 +1,6 @@
-import glob
 import logging
 import os
+import pathlib
 import shutil
 import sys
 
@@ -11,52 +11,56 @@ import numpy as np
 logger = logging.getLogger(__name__)
 
 
-def flip_headers(sesDirectory, outputDirectory):
-    SessionOutputDir = os.path.join(outputDirectory, sesDirectory.split("/")[-2])
-    if not os.path.exists(SessionOutputDir):
-        os.mkdir(SessionOutputDir)
+REORIENT_MATRIX = np.array([[1, 0, 0, 0], [0, 0, -1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
 
-    modalitiesDirectory = glob.iglob(os.path.join(sesDirectory, "*"))
-    session = sesDirectory.split("/")[-2]
-    logger.info("Processing session %s", session)
-    for modality in modalitiesDirectory:
-        ModOutputDir = os.path.join(outputDirectory, session, modality.split("/")[-1])
-        if not os.path.exists(ModOutputDir):
-            os.mkdir(ModOutputDir)
 
-        filenamesDirectory = glob.iglob(os.path.join(modality, "*.nii.gz"))
-        logger.info("Processing modality %s", modality.split("/")[-1])
-        for volume in filenamesDirectory:
-            logger.info("Converting %s", volume.split("/")[-1])
-            outputFilename = os.path.join(
-                outputDirectory,
-                session,
-                modality.split("/")[-1],
-                volume.split("/")[-1],
-            )
-            if not os.path.exists(outputFilename):
-                meta = ni.load(volume)
-                matRotation = np.array(
-                    [[1, 0, 0, 0], [0, 0, -1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]
-                )
+def flip_headers(
+    sesDirectory: str | os.PathLike, outputDirectory: str | os.PathLike
+) -> None:
+    input_session_dir = pathlib.Path(sesDirectory).resolve()
+    output_bids_dir = pathlib.Path(outputDirectory)
+
+    if (
+        not input_session_dir.is_dir()
+        or not input_session_dir.name.startswith("ses-")
+        or not input_session_dir.parent.name.startswith("sub-")
+    ):
+        logger.fatal("Expecting a BIDS Session directory as first argument, aborting.")
+        return 1
+
+    # re-create sub-*/ses-*
+    output_session_dir = (
+        output_bids_dir / input_session_dir.parent.name / input_session_dir.name
+    )
+    output_session_dir.parent.mkdir(exist_ok=True)
+    output_session_dir.mkdir(exist_ok=True)
+
+    logger.info("Processing session %s", input_session_dir.name)
+    for datatype_input_dir in input_session_dir.iterdir():
+        if not datatype_input_dir.is_dir():
+            logger.info("Skipping non-directory entry: %s", datatype_input_dir)
+            continue
+        datatype = datatype_input_dir.name
+        output_datatype_dir = output_session_dir / datatype
+        output_datatype_dir.mkdir(exist_ok=True)
+
+        logger.info("Processing datatype %s", datatype)
+        for input_volume_path in datatype_input_dir.glob("*.nii*"):
+            input_volume_basename = input_volume_path.name.split(".")[0]
+            output_volume_path = output_datatype_dir / input_volume_path.name
+            input_json_path = datatype_input_dir / (input_volume_basename + ".json")
+            output_json_path = output_datatype_dir / input_json_path.name
+
+            if output_volume_path.exists() and output_json_path.exists():
+                logger.info("Skipping %s which already exists", input_volume_path.name)
+            else:
+                logger.info("Processing %s...", input_volume_path.name)
+                meta = ni.load(input_volume_path)
                 ni_im = ni.Nifti1Image(
-                    meta.get_fdata(), np.dot(matRotation, meta.affine), meta.header
+                    meta.get_fdata(), REORIENT_MATRIX @ meta.affine, meta.header
                 )
-                ni.save(ni_im, outputFilename)
-
-            JsonFilename = os.path.join(
-                sesDirectory,
-                modality.split("/")[-1],
-                (volume.split("/")[-1]).split(".")[0] + ".json",
-            )
-            outputJsonFilename = os.path.join(
-                outputDirectory,
-                session,
-                modality.split("/")[-1],
-                (volume.split("/")[-1]).split(".")[0] + ".json",
-            )
-            if os.path.exists(JsonFilename) and not os.path.exists(outputJsonFilename):
-                shutil.copy(JsonFilename, outputJsonFilename)
+                ni.save(ni_im, output_volume_path)
+                shutil.copy(input_json_path, output_json_path)
 
 
 def parse_command_line(argv):
