5ddc8bbd8bf9604317d9725e32666a248887b566
Yann Leprince <yann.leprince@cea.fr>
2026-01-19 14:40:02 +0100

    Change path to the gkg-diffusion-pipeline repository

--- phcp/config.py
+++ phcp/config.py
@@ -3,7 +3,7 @@ GKG_CONTAINER_PATHS = {
     "2022-12-20": "/neurospin/phcp/code/gkg/2022-12-20_gkg/2022-12-20_gkg.sif",
 }
 
-GKG_DIFFUSION_PIPELINE_PATH = "/neurospin/phcp/code/2023-01_gkg_relaxo/myproject"
+GKG_DIFFUSION_PIPELINE_PATH = "/neurospin/phcp/code/gkg-diffusion-pipeline"
 GKG_DIFFUSION_PIPELINE_ENTRYPOINT = "Myproject2.py"
 
 FSLDIR = "/drf/local/fsl-6.0.7.13"
